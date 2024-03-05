b450fb51493b32862040248ce453ae54d0ae1e14
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-03-05 18:57:26 -0500

    DB Rescute

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ config();
 //? process.env.NAME-DE-LA-ENV = Asi es como se leen las env
 
 export const PORT = process.env.PORT || 3000;
-export const DB_NAME = process.env.DB_NAME;
-export const DB_USER = process.env.DB_USER;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME || "fordmunstangsdb";
+export const DB_USER = process.env.DB_USER || "b9yarsdikp4t7i0j4fy8";
+export const DB_HOST = process.env.DB_HOST || "us-east.connect.psdb.cloud";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "pscale_pw_Sa1UNr2wR0DcbZ1k45Ky3PYa6LbbWlLhJ9RkG74NXXu";
 export const DB_PORT = process.env.DB_PORT;
